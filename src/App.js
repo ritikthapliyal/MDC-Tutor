924358d24d4193bcf69ce1e4bebdacf27f36d529
ritikthapliyal <ritikthapliyal123@gmail.com>
2023-01-20 17:06:19 +0530

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 // import { LoginBg } from "./auth/LoginBg";
 // import { Basicdetails } from "./Components/Registration/Basicdetails";
 
-// import ProfileUnlocking from './Components/ProfileUnlocking/ProfileUnlocking'
-// import Teacher_Dashboard from "./Components/Teacher-Dashboard/Teacher_DashBoard";
+import ProfileUnlocking from './Components/ProfileUnlocking/ProfileUnlocking'
+import Teacher_Dashboard from "./Components/Teacher-Dashboard/Teacher_DashBoard";
 
 
 function App() {
@@ -12,8 +12,8 @@ function App() {
  <>
   <Router>
       <Routes>
-         {/* <Route exact path="/" element={<Teacher_Dashboard/>}/> */}
-         {/* <Route exact path="/" element={<ProfileUnlocking/>}/> */}
+         <Route exact path="/" element={<Teacher_Dashboard/>}/>
+         <Route exact path="/" element={<ProfileUnlocking/>}/>
       </Routes>
   </Router>
  </>

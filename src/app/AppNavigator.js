d6191ea77d32dfc9ee77b400a035de63862fee15
shreyansh770 <shreyanshthakur1@gmail.com>
2023-01-20 22:11:03 +0530

    route for dashboard added

--- src/app/AppNavigator.js
+++ src/app/AppNavigator.js
@@ -5,11 +5,13 @@ import {
     Route,
 } from "react-router-dom";
 import { Basicdetails } from "../Components/Registration/Basicdetails";
+import Teacher_DashBoard from "../Components/Teacher-Dashboard/Teacher_DashBoard.js"
 function AppNavigator(props) {
     return (
         <Router>
             <Routes>
                 <Route exact path="/" element={<Basicdetails/>}/>
+                <Route exact path="/dashboard" element={<Teacher_DashBoard/>}/>
             </Routes>
         </Router>
     );
